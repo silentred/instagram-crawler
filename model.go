703a1ed61bfc35085b3e2035f83fea8728e82f17
Jason W <silentred@163.com>
2015-08-21 17:48:22 +0800

    make db global.. not cool

--- model.go
+++ model.go
@@ -1,17 +1,33 @@
 package main
 
+import (
+	"log"
+)
 
-type Picture struct{
-    Id string   `db:"orig_id"`
-    Url string  `db:"pic_url"`
-    Status int   `db:"status"`
-    CreatedTime int `db:"created_time"`
+type Picture struct {
+	Id          string `db:"orig_id"`
+	Url         string `db:"pic_url"`
+	Status      int    `db:"status"`
+	CreatedTime int    `db:"created_time"`
 }
 
-type User struct{
-    Id string   `db:"orig_id"`
-    Name string `db:"name"`
-    AccessToken string  `db:"access_token"`
-    LastAuthTime int    `db:"last_auth_time"`
-    Valid int   `db:"valid"`
+type User struct {
+	Id           string `db:"orig_id"`
+	Name         string `db:"name"`
+	AccessToken  string `db:"access_token"`
+	LastAuthTime int    `db:"last_auth_time"`
+	Valid        int    `db:"valid"`
+}
+
+func (user User) Insert() int {
+	sql := "insert into user (orig_id, name, access_token, last_auth_time, valid) values (:orig_id, :name, :access_token, :last_auth_time, :valid)"
+	result, err := mysqlDB.NamedExec(sql, &user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return int(rowsAffected)
 }
