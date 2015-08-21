7ed694e596ed4c3c386abb542fb5a0d7b69a7a7b
Jason W <silentred@163.com>
2015-08-21 23:57:00 +0800

    add testing

---
+++ helper_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+/* Test Helpers */
+func expect(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
+}
+
+func refute(t *testing.T, a interface{}, b interface{}) {
+	if a == b {
+		t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
+}
+
+func TestDerterminPath(t *testing.T) {
+	expect(t, 1, 1)
+}
+
+func TestDownload(t *testing.T) {
+
+}
